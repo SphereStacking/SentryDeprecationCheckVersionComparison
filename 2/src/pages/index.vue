2df88f728c5236fa16b6c1565d678a1c88058ffb
SphereStacking <sphere.stacking@gmail.com>
2024-05-18 17:58:26 +0900

    ^つけてメジャーバージョン未満の最新バージョンに自動でアップデート

---
+++ 2/src/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    2
+    <button @click="handleClick">
+      Test Error Capture Exception
+    </button>
+    <button @click="addEventListenerUnload">
+      Test Unload
+    </button>
+    <a href="/">
+      Test Error Capture Exception
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    handleClick() {
+      console.log('handleClick')
+      this.$sentry.captureException(new Error('Sentry test error'));
+    },
+    addEventListenerUnload() {
+      window.addEventListener('unload', () => {
+        console.log('unload')
+      })
+    }
+  }
+}
+</script>
