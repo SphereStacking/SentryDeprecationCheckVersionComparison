2bdb9627012909f6913965126a26f6eadd3dae9d
SphereStacking <sphere.stacking@gmail.com>
2024-05-18 17:15:24 +0900

    バージョンを nuxt 2.15.2 sentry 5.1.5に固定

---
+++ 1/src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="handleClick">
+      Test Error Capture Exception
+    </button>
+    <button @click="addEventListenerUnload">
+      Test Unload
+    </button>
+    <a href="/">
+      Test Error Capture Exception
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    handleClick() {
+      console.log('handleClick')
+      this.$sentry.captureException(new Error('Sentry test error'));
+    },
+    addEventListenerUnload() {
+      window.addEventListener('unload', () => {
+        console.log('unload')
+      })
+    }
+  }
+}
+</script>
